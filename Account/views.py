773327773ae512838a902bc61c29fb9ac6d61c50
NumberJamie <jamievanessen@gmail.com>
2023-05-21 16:13:40 +0200

    account detail (#6)

--- Account/views.py
+++ Account/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView
 
-# Create your views here.
+from Account.models import Account
+
+
+class AccountDetailView(LoginRequiredMixin, DetailView):
+    model = Account
+    template_name = 'account/detail.html'
+
+    def get_object(self, queryset=None):
+        return Account.objects.get(username=self.kwargs.get('username'))
--- Cashew/urls.py
+++ Cashew/urls.py
@@ -18,10 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from Account.views import AccountDetailView
 from Cashew import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/<str:username>/', AccountDetailView.as_view(), name='account_detail'),
 ]
 
 if settings.DEBUG:
